52b4a20e2a2a2e3094ba981b4dc5c3416a3d020c
Mark Ryan <markryandeveloper@gmail.com>
2020-04-09 22:23:09 -0400

    added chartjs feature - bugs still remain with initial load of site

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const updated = document.getElementById('updated');
 const delta = 60000;
 
 function renderOptionsLists() {
-  let list = JSON.parse(localStorage.getItem("data"));
+  let list = getDataFromStorage();
   let options = Object.keys(list.rates).map(code => {
     return `<option value="${code}">${code}</option>`;
   })
@@ -16,30 +16,66 @@ function renderOptionsLists() {
   currencyElementTwo.innerHTML = options;
 }
 
+// Get date ranges for chart data
+const getDaysInMonth = (year, month) => new Date(year, month, 0).getDate()
+
+const addMonths = (input, months) => {
+  const date = new Date(input)
+  date.setDate(1)
+  date.setMonth(date.getMonth() + months)
+  date.setDate(Math.min(input.getDate(), getDaysInMonth(date.getFullYear(), date.getMonth()+1)))
+  return date.toISOString().slice(0,10);
+}
+
+// Fetch chart json data
+function fetchChartData() {
+  let now = new Date();
+  const today = new Date();
+  const start = addMonths(today, -7);
+  const end = addMonths(today, 0);
+  const currencyOneValue = currencyElementOne.value;
+  fetch(`https://api.exchangeratesapi.io/history?start_at=${start}&end_at=${end}&base=${currencyOneValue}`)
+    .then(res => res.json())
+    .then(chartJson => {
+      localStorage.setItem(`${currencyOneValue}ChartData`, JSON.stringify(chartJson));
+      localStorage.setItem(`${currencyOneValue}ChartTimestamp`, now);
+    })
+}
+
 // Fetch json data from api resource with base currency
 function fetchData() {
   let now = new Date().getTime();
-  const currencyOneValue = currencyElementOne.value;
+  let currencyOneValue = currencyElementOne.value;
   
   console.log(`fetching ${currencyElementOne.value} from api.exchangeratesapi.io`)
   fetch(`https://api.exchangeratesapi.io/latest?base=${currencyOneValue}`)
     .then(res => res.json())
     .then(myJson => {
-      localStorage.setItem("data", JSON.stringify(myJson));
-      localStorage.setItem("timestamp", now+delta);
+      localStorage.setItem(`${currencyOneValue}Data`, JSON.stringify(myJson));
+      localStorage.setItem(`${currencyOneValue}Timestamp`, now+delta);
     })
-    .then(calculate)
 }
 
 // Try to get data from cache, update data in cache if need be
 function getDataFromStorage() {
   let now = new Date().getTime();
-  if (localStorage.getItem("timestamp") < now) {
+  if (!localStorage.getItem(`${currencyElementOne.value}Data`) || localStorage.getItem(`${currencyElementOne.value}Timestamp`) < now) {
       fetchData();
     } else {
-      console.log(`fetching ${currencyElementTwo.value} from local storage`)
+      console.log(`fetching from local storage`)
     }
-  return JSON.parse(localStorage.getItem("data")); 
+  return JSON.parse(localStorage.getItem(`${currencyElementOne.value}Data`)); 
+  }
+
+// Try to get CHART data from cache, update data in cache if need be
+function getChartDataFromStorage() {
+  let now = new Date();
+  if (!localStorage.getItem(`${currencyElementOne.value}ChartData`) || localStorage.getItem(`${currencyElementOne.value}ChartTimestamp`) !== now) {
+      fetchChartData();
+    } else {
+      console.log(`fetching chart data from local storage`)
+    }
+  return JSON.parse(localStorage.getItem(`${currencyElementOne.value}ChartData`)); 
   }
 
 // Get exchange rate and render currency values
@@ -52,7 +88,7 @@ function calculate() {
   console.log("calculating...");
   rateElement.innerText = `1 ${currencyOneValue} = ${rate} ${currencyTwoValue}`;
   amountElementTwo.value = (amountElementOne.value * rate).toFixed(2);
-  let time = localStorage.getItem("timestamp") - delta
+  let time = localStorage.getItem(`${currencyOneValue}Timestamp`) - delta
   updated.innerText = `rates updated ${(now - time)/1000} seconds ago`;
   console.log("done")
 }
@@ -65,17 +101,127 @@ function swap() {
   currencyElementTwo.value = temp;
 }
 
-renderOptionsLists()
-calculate()
+// Initialize
+renderOptionsLists();
+calculate();
+
+
+// Chart JS
+function getKeys() {
+  let s =  getChartDataFromStorage().rates;
+  let keys = [];
+  let months = [];
+  for(var k in s) {
+    if (k.slice(8,10) === '01' && !months.includes(k.slice(5,7))) {
+      keys.push(k);
+      months.push(k.slice(5,7));
+    } else if (k.slice(8,10) === '03' && !months.includes(k.slice(5,7))) {
+      keys.push(k);
+      months.push(k.slice(5,7));
+    }
+  }
+  return keys.sort()
+}
+
+function getValues() {
+  let s = getChartDataFromStorage().rates;
+  let keys = getKeys();
+  let vals = [];
+  for(var k in keys) {
+    vals.push(s[keys[k]][currencyElementTwo.value])
+  }
+  return vals;
+}
+
+function addData(chart, data) {
+  chart.data.datasets[0].data = data();
+  chart.data.datasets[0].label = `${currencyElementOne.value}/${currencyElementTwo.value}`;
+  chart.update();
+}
+
+function removeData(chart) {
+  chart.data.datasets[0].data = [];
+  chart.data.datasets[0].label = '';
+  chart.update();
+}
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: getKeys(),
+      datasets: [{
+          label: `${currencyElementOne.value}/${currencyElementTwo.value}`,
+          data: [],
+          backgroundColor: 'rgba(95,186,167,0)',
+          borderWidth: 3,
+          borderColor: 'rgba(95,186,167,.3)'
+      }]
+  },
+  options: {
+      layout: {
+        padding: {
+          right: 30,
+          left: 5,
+          top: 10,
+          bottom: 10,
+        }
+      },
+      scales: {
+          yAxes: [{
+              borderColor: 'Red',
+              display: false,
+              gridLines: {
+                color: '',
+                zeroLineColor: 'Red',
+                drawTicks: false,
+              },
+              ticks: {
+                  beginAtZero: false
+              }
+          }],
+          xAxes: [{
+              display: false,
+              lineWidth: 0,
+              drawTicks: true,
+              gridLines: {
+                color: '',
+                zeroLineColor: '',
+                drawBorder: false,
+              },
+              ticks: {
+                fontColor: '#d3d3d3',
+                minRotation: 90,
+              }     
+          }]
+      },
+    legend: {
+      display: false
+    }
+  }
+});
 
 // Event listeners
 currencyElementOne.addEventListener('change', () => {
-  fetchData();
+  getDataFromStorage();
+  getChartDataFromStorage();
+  removeData(myChart);
+  addData(myChart, getValues);
 });
-amountElementOne.addEventListener('input', calculate);
-currencyElementTwo.addEventListener('change', calculate);
+amountElementOne.addEventListener('input', () => {
+  calculate();
+});
+currencyElementTwo.addEventListener('change', () => {
+  getChartDataFromStorage();
+  calculate();
+  removeData(myChart);
+  addData(myChart, getValues);
+});
 amountElementTwo.addEventListener('input', calculate);
 swapButton.addEventListener('click', () => {
   swap();
-  fetchData();
+  getDataFromStorage();
+  getChartDataFromStorage();
+  removeData(myChart);
+  addData(myChart, getValues);
 });
